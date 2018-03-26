f3f4350040554b776de4ad64fb09d22171fbd901
Christoph Herr <info@christophherr.com>
2018-03-26 15:02:07 -0400

    ES6ed standard JS file.

--- js/prometheus2.js
+++ js/prometheus2.js
@@ -1,12 +1,12 @@
 /**
- * Prometheus2 entry point.
+ * Prometheus 2 entry point.
  *
  * @package ChristophHerr\Prometheus2\JS
  * @author  Christoph Herr
  * @license GPL-2.0+
  */
 
-var genesisSample = ( function( $ ) {
+const prometheus2 = ( function( $ ) {
 	'use strict';
 
 	/**
@@ -14,48 +14,50 @@ var genesisSample = ( function( $ ) {
 	 *
 	 * @since 2.6.0
 	 */
-	var moveContentBelowFixedHeader = function() {
-		var siteInnerMarginTop = 0;
+	const moveContentBelowFixedHeader = function() {
+			let siteInnerMarginTop = 0;
+			const siteHeader = $( document.getElementsByClassName( 'site-header' ) );
+			const siteInner = $( document.getElementsByClassName( 'site-inner' ) );
 
-		if( $('.site-header').css('position') === 'fixed' ) {
-			siteInnerMarginTop = $('.site-header').outerHeight();
-		}
+			if ( 'fixed' === siteHeader.css( 'position' ) ) {
+				siteInnerMarginTop = siteHeader.outerHeight();
+			}
 
-		$('.site-inner').css('margin-top', siteInnerMarginTop);
-	},
+			siteInner.css( 'margin-top', siteInnerMarginTop );
+		},
 
-	/**
-	 * Initialize Genesis Sample.
-	 *
-	 * Internal functions to execute on document load can be called here.
-	 *
-	 * @since 2.6.0
-	 */
-	init = function() {
-		// Run on first load.
-		moveContentBelowFixedHeader();
+		/**
+		 * Initialize Genesis Sample.
+		 *
+		 * Internal functions to execute on document load can be called here.
+		 *
+		 * @since 2.6.0
+		 */
+		init = () => {
 
-		// Run after window resize.
-		$( window ).resize(function() {
+			// Run on first load.
 			moveContentBelowFixedHeader();
-		});
 
-		// Run after the Customizer updates.
-		// 1.5s delay is to allow logo area reflow.
-		if (typeof wp.customize != "undefined") {
-			wp.customize.bind( 'change', function ( setting ) {
-				setTimeout(function() {
-					moveContentBelowFixedHeader();
-				  }, 1500);
+			// Run after window resize.
+			$( window ).resize( () => {
+				moveContentBelowFixedHeader();
 			});
-		}
-	};
+
+			// Run after the Customizer updates.
+			// 1.5s delay is to allow logo area reflow.
+			if ( 'undefined' !== typeof wp.customize ) {
+				wp.customize.bind( 'change', setting => {
+					setTimeout( () => {
+						moveContentBelowFixedHeader();
+					}, 1500 );
+				});
+			}
+		};
 
 	// Expose the init function only.
 	return {
-		init: init
+		init
 	};
+}( jQuery ) );
 
-})( jQuery );
-
-jQuery( window ).on( 'load', genesisSample.init );
+jQuery( window ).on( 'load', prometheus2.init );
