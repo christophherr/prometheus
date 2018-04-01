73f28512c708c8e96c50d3cea0ec868f3fa8837c
Christoph Herr <info@christophherr.com>
2018-04-01 12:22:13 -0400

    Improved gulpfile.js, added gulp-stylelint.

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,23 @@
  */
 
 // Set up dependencies.
-const autoprefixer = require( 'autoprefixer' ),
-	del = require( 'del' ),
-	mqpacker = require( 'css-mqpacker' ),
-	fs = require( 'fs' ),
-	gulp = require( 'gulp' ),
-	cleancss = require( 'gulp-clean-css' ),
-	cssnano = require( 'gulp-cssnano' ),
-	notify = require( 'gulp-notify' ),
-	pixrem = require( 'gulp-pixrem' ),
-	plumber = require( 'gulp-plumber' ),
-	postcss = require( 'gulp-postcss' ),
-	rename = require( 'gulp-rename' ),
-	sass = require( 'gulp-sass' ),
-	sassLint = require( 'gulp-sass-lint' ),
-	sourcemaps = require( 'gulp-sourcemaps' ),
-	stylefmt = require( 'gulp-stylefmt' );
+const autoprefixer = require( 'autoprefixer' );
+const del = require( 'del' );
+const mqpacker = require( 'css-mqpacker' );
+const fs = require( 'fs' );
+const gulp = require( 'gulp' );
+const cleancss = require( 'gulp-clean-css' );
+const cssnano = require( 'gulp-cssnano' );
+const notify = require( 'gulp-notify' );
+const pixrem = require( 'gulp-pixrem' );
+const plumber = require( 'gulp-plumber' );
+const postcss = require( 'gulp-postcss' );
+const rename = require( 'gulp-rename' );
+const sass = require( 'gulp-sass' );
+const sassLint = require( 'gulp-sass-lint' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+const stylefmt = require( 'gulp-stylefmt' );
+const styleLint = require( 'gulp-stylelint' );
 
 /**
  * Error handling
@@ -30,18 +31,14 @@ const autoprefixer = require( 'autoprefixer' ),
  * @function
  */
 function handleErrors() {
-	var args = Array.prototype.slice.call( arguments );
+	const args = Array.prototype.slice.call( arguments );
 
 	notify
 		.onError({
 			title: 'Task Failed [<%= error.message %>',
 			message: 'See console.'
-
-			//sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
 		})
 		.apply( this, args );
-
-	//gutil.beep(); // Beep 'sosumi' again
 
 	// Prevent the 'watch' task from stopping
 	this.emit( 'end' );
@@ -54,114 +51,116 @@ function handleErrors() {
 /**
  * PostCSS Task Handler
  */
-gulp.task( 'postcss', function() {
-	return (
-		gulp
-			.src( 'scss/style.scss' )
+gulp.task( 'postcss', () => {
+	gulp
+		.src( 'scss/style.scss' )
 
-			// Error handling
-			.pipe(
-				plumber({
-					errorHandler: handleErrors
+		// Error handling
+		.pipe(
+			plumber({
+				errorHandler: handleErrors
+			})
+		)
+
+		// Wrap tasks in a sourcemap.
+		.pipe( sourcemaps.init() )
+
+		// Sass magic.
+		.pipe(
+			sass({
+				errLogToConsole: true,
+				outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
+			})
+		)
+
+		// Pixel fallbacks for rem units.
+		.pipe( pixrem() )
+
+		// PostCSS magic.
+		.pipe(
+			postcss([
+				autoprefixer({
+					browsers: [ 'last 2 versions' ]
+				}),
+				mqpacker({
+					sort: true
 				})
-			)
+			])
+		)
 
-			// Wrap tasks in a sourcemap.
-			.pipe( sourcemaps.init() )
+		// Change spaces to tabs and other fixes.
+		.pipe( stylefmt() )
 
-			// Sass magic.
-			.pipe(
-				sass({
-					errLogToConsole: true,
-					outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
-				})
-			)
+		// Additional WordPress style fixes.
+		.pipe(
+			styleLint({
+				fix: true
+			})
+		)
 
-			// Pixel fallbacks for rem units.
-			.pipe( pixrem() )
-
-			// PostCSS magic.
-			.pipe(
-				postcss([
-					autoprefixer({
-						browsers: [ 'last 2 versions' ]
-					}),
-					mqpacker({
-						sort: true
-					})
-				])
-			)
-
-			// Change spaces to tabs.
-			.pipe( stylefmt() )
-
-			// Create the source map.
-			.pipe(
-				sourcemaps.write( './', {
-					includeContent: false
-				})
-			)
-
-			.pipe( gulp.dest( './' ) )
-	);
+		// Create the source map.
+		.pipe(
+			sourcemaps.write( './', {
+				includeContent: false
+			})
+		)
+		.pipe( gulp.dest( './' ) );
 });
 
-gulp.task( 'rename:map', [ 'postcss' ], function() {
-	return gulp
+gulp.task( 'rename:map', [ 'postcss' ], () => {
+	gulp
 		.src( 'style.css.map' )
 		.pipe( rename( 'style.min.css.map' ) )
 		.pipe( gulp.dest( './' ) );
 });
 
-gulp.task( 'clean:map', [ 'rename:map' ], function() {
+gulp.task( 'clean:map', [ 'rename:map' ], () => {
 	return del( 'style.css.map' );
 });
 
-gulp.task( 'css:minify', [ 'clean:map' ], function() {
-	return (
-		gulp
-			.src( 'style.css' )
+gulp.task( 'css:minify', [ 'clean:map' ], () => {
+	gulp
+		.src( 'style.css' )
 
-			// Error handling
-			.pipe(
-				plumber({
-					errorHandler: handleErrors
-				})
-			)
+		// Error handling
+		.pipe(
+			plumber({
+				errorHandler: handleErrors
+			})
+		)
 
-			// Combine similar rules.
-			.pipe(
-				cleancss({
-					level: {
-						2: {
-							all: true
-						}
+		// Combine similar rules.
+		.pipe(
+			cleancss({
+				level: {
+					2: {
+						all: true
 					}
-				})
-			)
+				}
+			})
+		)
 
-			// Minify and optimize style.css again.
-			.pipe(
-				cssnano({
-					safe: false,
-					discardComments: {
-						removeAll: true
-					}
-				})
-			)
+		// Minify and optimize style.css again.
+		.pipe(
+			cssnano({
+				safe: false,
+				discardComments: {
+					removeAll: true
+				}
+			})
+		)
 
-			.pipe( rename( 'style.min.css' ) )
-			.pipe( gulp.dest( './' ) )
+		.pipe( rename( 'style.min.css' ) )
+		.pipe( gulp.dest( './' ) )
 
-			.pipe(
-				notify({
-					message: 'Styles are built.'
-				})
-			)
-	);
+		.pipe(
+			notify({
+				message: 'Styles are built.'
+			})
+		);
 });
 
-gulp.task( 'sass:lint', [ 'css:minify' ], function() {
+gulp.task( 'sass:lint', [ 'css:minify' ], () => {
 	gulp
 		.src([ '/scss/style.scss', '!/scss/resets/index.scss' ])
 		.pipe( sassLint() )
@@ -173,7 +172,7 @@ gulp.task( 'sass:lint', [ 'css:minify' ], function() {
  * All Tasks Listeners
  *******************/
 
-gulp.task( 'watch', function() {
+gulp.task( 'watch', () => {
 	gulp.watch( 'scss/**/*.scss', [ 'styles' ]);
 });
 
